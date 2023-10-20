4ade20110efb55a8923a782e383fa61da984d3a2
はらなおみ <haraweb7@gmail.com>
2023-10-20 12:09:36 +0900

    投稿確認機能完成

--- app/controllers/picture_posts_controller.rb
+++ app/controllers/picture_posts_controller.rb
@@ -1,20 +1,20 @@
 class PicturePostsController < ApplicationController
   def index
-    @picture_posts = Picture_post.all
+    @picture_posts = PicturePost.all
   end
 
   def new
-    @picture_post = Picture_post.new
+    @picture_post = PicturePost.new
   end
 
   def confirm
-    @picture_post = Picture_post.new(post_params)
+    @picture_post = PicturePost.new(post_params)
     @picture_post.user_id = current_user.id 
     render :new if @picture_post.invalid?
   end
 
   def create
-    @picture_post = Picture_post.new(post_params)
+    @picture_post = PicturePost.new(post_params)
     @picture_post.user_id = current_user.id 
     if params[:back]
       render :new
@@ -28,15 +28,15 @@ class PicturePostsController < ApplicationController
   end
 
   def show
-    @picture_post = Picture_post.find(params[:id])
+    @picture_post = PicturePost.find(params[:id])
   end
 
   def edit
-    @picture_post = Picture_post.find(params[:id])
+    @picture_post = PicturePost.find(params[:id])
   end
 
   def update
-    @picture_post = Picture_post.find(params[:id])
+    @picture_post = PicturePost.find(params[:id])
     if @picture_post.update(post_params)
       redirect_to picture_posts_path, notice: "編集しました！"
     else
@@ -45,7 +45,7 @@ class PicturePostsController < ApplicationController
   end
 
   def destroy
-    @picture_post = Picture_post.find(params[:id])
+    @picture_post = PicturePost.find(params[:id])
     @picture_post.destroy
     redirect_to picture_posts_path, notice:"削除しました！"
   end
@@ -58,4 +58,4 @@ class PicturePostsController < ApplicationController
 end
 
 
-end
+
