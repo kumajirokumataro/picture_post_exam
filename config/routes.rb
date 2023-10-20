f1a679431e5ab055592093668354647d750cde73
はらなおみ <haraweb7@gmail.com>
2023-10-20 16:44:13 +0900

    ユーザー登録機能完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :update, :edit]
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy, :index]
   resources :picture_posts do
     collection do
       post :confirm
