a79639313172dea9b55b4480a03622b33f3ee8da
はらなおみ <haraweb7@gmail.com>
2023-10-22 12:36:07 +0900

    ユーザー編集機能完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to picture_posts_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+
     private
 
     def user_params
